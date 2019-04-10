203500144a558edf0d72215310621f40b2f77243
daandymaan <sanieldimons@gmail.com>
2019-04-10 16:20:47 +0100

    created Colours class with getters and setters

---
+++ src/ie/tudublin/Colours.java
@@ -0,0 +1,17 @@
+package ie.tudublin;
+
+
+
+public class Colours
+{
+    private String colour;
+    private int rgb;
+    private float value;
+
+    public Colours(String colour, int rgb, float value)
+    {
+        this.colour = colour;
+        this.rgb = rgb;
+        this.value = value;
+    }
+}

bdbaa4728f43d4772a40498e3b7b4cbbf4573982
daandymaan <sanieldimons@gmail.com>
2019-04-10 16:21:07 +0100

    created Colours class with getters and setters v2

--- src/ie/tudublin/Colours.java
+++ src/ie/tudublin/Colours.java
@@ -14,4 +14,46 @@ public class Colours
         this.rgb = rgb;
         this.value = value;
     }
+
+    /**
+     * @return the colour
+     */
+    public String getColour() {
+        return colour;
+    }
+
+    /**
+     * @param colour the colour to set
+     */
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    /**
+     * @return the rgb
+     */
+    public int getRgb() {
+        return rgb;
+    }
+
+    /**
+     * @param rgb the rgb to set
+     */
+    public void setRgb(int rgb) {
+        this.rgb = rgb;
+    }
+
+    /**
+     * @return the value
+     */
+    public float getValue() {
+        return value;
+    }
+
+    /**
+     * @param value the value to set
+     */
+    public void setValue(float value) {
+        this.value = value;
+    }
 }
